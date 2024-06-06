561e288f544ebc422f91ed7c633aebca3f1c8ae5
Enrico Mosca <20000enrico@gmail.com>
2024-06-06 11:42:00 +0200

    Finito

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -1,48 +1,119 @@
 package org.generation.italy;
-import org.generation.italy.model.Televisore;
 
+import org.generation.italy.model.Televisore;
 
 import java.util.Scanner;
 
 public class Main {
 
+	private static Televisore tv;
+	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
+
 		
-		Scanner sc = new Scanner(System.in);
 		String marca, modello;
-		int scelta = 0;
-		Televisore tv;
+		int scelta, nrCanale = 0;
 		int nrPollici;
-		
-	do {
-		System.out.println("Menù");
-		scelta = sc.nextInt();
-		sc.nextLine();
-		switch (scelta) {
-		
-		case(1): 
-			System.out.println("Inserisci la marca: ");
-			marca = sc.nextLine();
-			System.out.println("Inserisci modello: ");
-			modello = sc.nextLine();
-			System.out.println("Inserisci numero pollici: ");
-			nrPollici = sc.nextInt();
-			System.out.println("Inserire la modalità hotel? Y/N");
-			boolean modalitaHotel = sc.nextBoolean();
-			sc.nextLine();
+
+		do {
 			
-		case(2):
-			tv.accendi();
-			System.out.println("Stato tv: " + tv.isStato());
-		case(3):
+			System.out.println(
+					"Menù: \n1. Crea TV \n2. Accendi TV \n3. Spegni TV \n4. Aumenta volume \n5. Diminuisci volume \n6. Cambia canale \n7. Mostra canale \n8. Visualizza elenco canali \n9. Esci");
+			scelta = sc.nextInt();
+			sc.nextLine();
+			switch (scelta) {
+			// Crea televisore
+			case 1:
+				try {
+					creaTelevisore();
+				} catch (Exception e) {
+					System.err.println("Errore: " + e.getMessage());
+				}
+				break;
+			// Accendi
+			case 2:
+				if (tv != null) {
+				tv.accendi();
+				System.out.println("Stato tv: " + tv.statoTV());
+				} else
+					System.out.println("La TV è inesistente!");
+				break;
+			// Spegni
+			case 3:
+				if (tv != null) {
+				tv.spegni();
+				System.out.println("Stato tv: " + tv.statoTV());
+				} else 
+					System.out.println("La TV è inesistente!");
+				break;
+			// Aumenta volume
+			case 4:
+				if (tv != null) {
+				tv.aumentaVolume();
+				System.out.println("Il volume è: " + tv.getVolume());
+				} else
+					System.out.println("La TV è inesistente");
+				break;
+			// Diminuisci volume
+			case 5:
+				if (tv != null) {
+				tv.diminuisciVolume();
+				System.out.println("Il volume è: " + tv.getVolume());
+				} else 
+					System.out.println("La TV è inesistente");
+				break;
+			// Cambia canale
+			case 6:
+				if (tv != null) {
+				nrCanale = sc.nextInt();
+				sc.nextLine();
+				tv.cambiaCanale(nrCanale);
+				} else
+					System.out.println("La TV è inesistente");
+				break;
+			// Mostra canale
+			case 7:
+				if (tv != null) {
+				System.out.println("Il canale corrente è: " + tv.getCanaleCorrente());
+				} else
+					System.out.println("La TV è inesistente");
+				break;
+			// Visualizza elenco canali
+			case 8:
+				if (tv != null) {
+				tv.getElencoCanali();
+				} else 
+					System.out.println("La TV è inesistente");
+				break;
+			// Esci
+			case 9:
+				break;
+			default:
+				System.out.println("Scelta non valida");
+			}
+			
+			if (tv != null)
+			System.out.println(tv.toString());
+			
+		} while (scelta != 9);
+
+	}
+
+	// Metodo per creazione televisore
+	private static void creaTelevisore() throws Exception {
+		System.out.println("Inserisci la marca: ");
+		String marca = sc.nextLine();
+		System.out.println("Inserisci il modello: ");
+		String modello = sc.nextLine();
+		System.out.println("Inserisci il numero dei pollici");
+		int nrPollici = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Inserire modalità hotel? true/false");
+		boolean modalitaHotel = Boolean.parseBoolean(sc.nextLine());
 		
-		}
-		
-		
-	} while (scelta == 9);
+		tv = new Televisore (marca, modello, nrPollici, modalitaHotel);
 		
 		
 	}
-
 }
--- src/org/generation/italy/model/Televisore.java
+++ src/org/generation/italy/model/Televisore.java
@@ -10,40 +10,69 @@ public class Televisore {
 	private int volume;
 	private boolean modalitaHotel;
 	private String[] elencoCanali;
-	private int canaleCorrente;
+	private String canaleCorrente;
 	private boolean cambiaCanale;
-	
-	
-	public Televisore (String marca, String modello, int nrPollici, boolean modalitaHotel) {
+
+	public Televisore(String marca, String modello, int nrPollici, boolean modalitaHotel) throws Exception {
 		// Inizializzazione attributi
-		this.marca = marca;
-		this.modello = modello;
-		this.nrPollici = nrPollici;
+		if (!marca.isEmpty())
+			this.marca = marca;
+		else
+			throw new Exception("marca non valida");
+		if (!modello.isEmpty())
+			this.modello = modello;
+		else
+			throw new Exception("modello non valido");
+		if (nrPollici > 0)
+			this.nrPollici = nrPollici;
+		else
+			throw new Exception("numero pollici non validi");
 		this.stato = false;
 		this.volume = 0;
-		this.modalitaHotel = false;
-		this.canaleCorrente = 0;
-		this.elencoCanali = new String[] 
-				{"Rai 1", "Rai 2", "Rai 3", "Rai 4", "Canale 5", "Italia 1", "La7", "TV8", "NOVE", "Italia 2"};
+		this.modalitaHotel = modalitaHotel;
+		this.canaleCorrente = "";
+		this.elencoCanali = new String[] { "Rai 1", "Rai 2", "Rai 3", "Rai 4", "Canale 5", "Italia 1", "La7", "TV8",
+				"NOVE", "Italia 2" };
 	}
-	
-	
+
 	public String getMarca() {
 		return marca;
 	}
-	
-	
-	// Metodo per l'accensione e lo spegnimento
-	
+
+	public String getModello() {
+		return modello;
+	}
+
+	public int getNrPollici() {
+		return nrPollici;
+	}
+
 	public boolean isStato() {
 		return stato;
 	}
-	
+
+	public boolean isModalitaHotel() {
+		return modalitaHotel;
+	}
+
+	public void setModalitaHotel(boolean modalitaHotel) {
+		this.modalitaHotel = modalitaHotel;
+	}
+
+	public boolean isCambiaCanale() {
+		return cambiaCanale;
+	}
+
+	// Metodi per l'accensione e lo spegnimento
+	public boolean statoTV() {
+		return stato;
+	}
+
 	public void accendi() {
 		this.stato = true;
-		this.canaleCorrente = 1;
+		this.canaleCorrente = "";
 	}
-	
+
 	public void spegni() {
 		this.stato = false;
 	}
@@ -52,43 +81,66 @@ public class Televisore {
 	public int getVolume() {
 		return volume;
 	}
-	
+
 	public void aumentaVolume() {
-		if (modalitaHotel && volume > 5) {
+		if (modalitaHotel && volume >= 5) {
+			System.out.println("Volume massimo per la modalità hotel");
 			this.volume = 5;
-		} else if (volume < 0 ) {
-			this.volume = 0;
-		}  
-		this.volume++;
-	}	
-	
-	
-	public void diminuisciVolume() {
-			if (modalitaHotel && volume > 5) {
-				this.volume = 5;
-			} else if (volume < 0 ) {
-				this.volume = 0;
-			}  
-			this.volume--;	
-	}
-	
-	// Metodi per i canali
-	public String[] getElencoCanali() {
-		return elencoCanali;
-	}
-	
-	public void cambiaCanale(int canale) {
-		if (canale >= 0 && canale <= this.elencoCanali.length) {
-			System.out.println("Cambiando il canale: " + this.elencoCanali[canale]);
-			this.canaleCorrente = (canale);
+		} else if (volume < 10 && stato) {
+			this.volume++;
+		} else if (!stato) {
+			System.out.println("La TV è spenta");
 		}
-		
+		if (volume == 10) {
+			System.out.println("Hai raggiunto il volume massimo!");
+		}
+
 	}
 
+	public void diminuisciVolume() {
+		if (volume > 0 && stato) {
+			this.volume--;
+		} else if (!stato) {
+			System.out.println("La TV è spenta");
+		}
+		if (volume == 0) {
+			System.out.println("Volume muto");
+		}
+	}
+
+	// Metodi per i canali
+	public void getElencoCanali() {
+		if (stato) {
+		System.out.println("Elenco canali: ");
+		for (String canale : elencoCanali) {
+			System.out.print(canale + " | ");
+		}
+		System.out.println();
+		} else
+			System.out.println("La TV è spenta");
+	}
+
+	public void cambiaCanale(int canale) {
+		if (stato) {
+			if (canale >= 0 && canale <= this.elencoCanali.length) {
+				System.out.println("Cambiando il canale: " + this.elencoCanali[canale]);
+				this.canaleCorrente = this.elencoCanali[canale];
+			} else {
+				System.out.println("Il canale non esiste.");
+			}
+		} else
+			System.out.println("La TV è spenta");
+	}
+
+	public String getCanaleCorrente() {
+		return canaleCorrente;
+	}
+
+	// Stampa TV
 	@Override
 	public String toString() {
 		return "Televisore [marca=" + marca + ", modello=" + modello + ", nrPollici=" + nrPollici + ", stato=" + stato
 				+ ", volume=" + volume + ", modalitaHotel=" + modalitaHotel + "]";
 	}
-	
+
 }
